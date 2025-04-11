f10f4f07b5ff73e3e8f354e8b39b358c9733cd3c
Nehan Yaser <67751528+webdevsint@users.noreply.github.com>
2025-04-11 21:55:12 +0600

    March Dump

---
+++ scripts/frames.js
@@ -0,0 +1,45 @@
+screen.orientation.lock();
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("frame");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+
+function viewFrames() {
+  document.querySelector(".content").style.display = "flex";
+  document.querySelector("audio").play();
+  document.querySelector(".content").scrollIntoView({ behavior: "smooth" });
+
+  setTimeout(() => {
+    document.querySelector(".loader").style.display = "none";
+    fadeInOut();
+  }, 800)
+}
+
+function fadeInOut() {
+  // Fade In
+  document.querySelector('.now-playing').style.opacity = 1;
+
+  // Wait for 10 seconds
+  setTimeout(() => {
+    // Fade Out
+    document.querySelector('.now-playing').style.opacity = 0;
+  }, 8000); // 10000 milliseconds = 10 seconds
+}
